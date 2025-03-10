48fcef5a585570c6d2d0a005ec2803348e7e492e
federicofanini <federico.fanini9@gmail.com>
2025-03-10 11:29:58 +0100

    exercises list

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -87,3 +87,14 @@ export function formatDate(date: string) {
     return `${fullDate} (${yearsAgo}y ago)`;
   }
 }
+
+export function slugify(text: string) {
+  return text
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, "-") // Replace spaces with -
+    .replace(/[^\w\-]+/g, "") // Remove all non-word chars
+    .replace(/\-\-+/g, "-") // Replace multiple - with single -
+    .replace(/^-+/, "") // Trim - from start of text
+    .replace(/-+$/, ""); // Trim - from end of text
+}
